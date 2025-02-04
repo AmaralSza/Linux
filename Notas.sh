bbbb9133c1b486dff2a5c6301a37dec629fb62ac
William Amaral de Souza <amaral.sza@gmail.com>
2025-02-04 19:43:39 -0300

    Update Notas.sh

--- Notas.sh
+++ Notas.sh
@@ -10,3 +10,16 @@ cd /home/linux/iventoy-1.0.20 && sh ./iventoy.sh -R start
 
 crontab -e
 @reboot sleep 10 && sh /root/iventoy-helper.sh
+
+
+================================================================================
+### ANYDESK NAO LIBERA ACESSO NAO SUPERVISIONADO
+
+# Editar o arquivo:
+sudo nano /etc/anydesk/system.conf 
+
+# Adicionar a linha ao final:
+ad.security.interactive_access=1
+
+# Rodar:
+anydesk-global-settings
